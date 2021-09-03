a5b9826cadb3f88f4c4ded260fdb7ad89bccbbc8
jurka14 <56232436+jurka14@users.noreply.github.com>
2021-09-03 16:49:00 +0200

    Add files via upload

---
+++ projects/imp_FreeRTOS/Sources/frtos_tasks.c
@@ -0,0 +1,23 @@
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.5 [05.21]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
+
+/* Begin of <includes> initialization, DO NOT MODIFY LINES BELOW */
+
+#include "FRTOS1.h"
+#include "frtos_tasks.h"
+
+/* End <includes> initialization, DO NOT MODIFY LINES ABOVE */
+
+void CreateTasks(void) {
+}
+
